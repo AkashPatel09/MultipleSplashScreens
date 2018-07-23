c9d18f2d8ffbcb90022d9588fae692b59e2f4de3
Akash <akashpatel116@gmail.com>
2018-07-23 08:25:32 +0530

    Basic Code
    Basic code show in the tutorial on youtube for setting multiple splash screens in your project/app.

---
+++ Multiple Splash/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Multiple Splash
+//
+//  Created by Akash Patel on 23/07/18.
+//  Copyright © 2018 Akash Patel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Multiple Splash/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Multiple Splash
+//
+//  Created by Akash Patel on 23/07/18.
+//  Copyright © 2018 Akash Patel. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
